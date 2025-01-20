b7eff42685871d340392bca6a7d9f882adc4589e
Dhruvi Ladani <110341143+dhruviladani@users.noreply.github.com>
2025-01-20 18:19:47 +0530

    Create Set .add().py

---
+++ Set .add().py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+s = set()
+# ans = []
+# for i in range(n):
+#     # s = s + s.add()
+#     ans = ans.add(s[i])
+for i in range(n):
+    s.add(input()) 
+
+
+print (len(s))
