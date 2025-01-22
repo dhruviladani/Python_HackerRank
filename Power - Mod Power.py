68d9de170424da96f18b382d3c450785a896627f
Dhruvi Ladani <110341143+dhruviladani@users.noreply.github.com>
2025-01-22 15:29:57 +0530

    Create Power - Mod Power.py

---
+++ Power - Mod Power.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+
+b = int(input())
+
+m = int(input())
+
+power = pow(a,b)
+print(power)
+
+power_mod = pow(a,b,m)
+print(power_mod)
