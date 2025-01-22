221ee1b4c17867e27f4aceb5e0dcfa0758172fe8
Dhruvi Ladani <110341143+dhruviladani@users.noreply.github.com>
2025-01-22 15:30:27 +0530

    Create Mod Divmod.py

---
+++ Mod Divmod.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+
+b = int(input())
+
+div = a // b 
+print(div)
+
+mod = a % b
+print(mod)
+
+d = divmod(a,b)
+print(d)
